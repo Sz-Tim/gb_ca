1446f5bd83f7aa9a74508892c730ab02d6655a3d
Sz-Tim <tim.szewczyk@gmail.com>
2017-09-15 13:47:52 -0600

    formatting

--- code/ca_fn.R
+++ code/ca_fn.R
@@ -13,38 +13,42 @@
 ##---
 ## simulation wrapper
 ##---
-run_sim <- function(
-  lc.mx,
-  N.init,
-  lambda,
-  K,
-  sdd.probs,
-  sdd.rate,
-  bird.pref,
-  pr.eat,
-  n.ldd,
-  tmax
-) {
-  # Runs the simulation, calling subsequent submodules
-  # 1. Initialize populations
-  N[,1] <- N.init
-  for(t in 2:tmax) {
-    # 2. Local fruit production
-    N.f <- make_fruits(lc.mx, N[,t], N.recruit[,t-1], fec, K)
-    # 3. Short distance dispersal
-    N.seed <- sdd_disperse(lc.mx, N.f, sdd.probs, bird.pref, pr.eat)
-    # 4. Long distance dispersal
-    N.seed <- ldd_disperse(lc.mx, N[,t], N.seed, n.ldd)
-    # 5. Seedling establishment
-    N.recruit[,t] <- new_seedlings(lc.mx, N.seed, pr.est)
-    # 6. Carrying capacity enforcement on adults
-    N[,t] <- pmin(N[,t], K)
-    N[,t+1] <- N[,t] + N.recruit[,t]
+  run_sim <- function(
+    lc.df,
+    N.init,
+    fec,
+    K,
+    sdd.probs,
+    sdd.rate,
+    bird.pref,
+    pr.eat,
+    n.ldd,
+    pr.est,
+    tmax
+  ) {
+    # Runs the simulation, calling subsequent submodules
+    # 1. Initialize populations
+    ncell <- nrow(lc.df)
+    N <- matrix(0, ncell, tmax)
+    N[,1] <- N.init
+    for(t in 2:tmax) {
+      # 2. Local fruit production
+      N.f <- make_fruits(lc.df, N[,t], N.recruit[,t-1], fec, K)
+      # 3. Short distance dispersal
+      N.seed <- sdd_disperse(lc.df, N.f, sdd.probs, bird.pref, pr.eat)
+      # 4. Long distance dispersal
+      N.seed <- ldd_disperse(lc.df, N[,t], N.seed, n.ldd)
+      # 5. Seedling establishment
+      N.recruit[,t] <- new_seedlings(lc.df, N.seed, pr.est)
+      # 6. Carrying capacity enforcement on adults
+      N[,t] <- pmin(N[,t], K)
+      N[,t+1] <- N[,t] + N.recruit[,t]
+    }
+    return(N)
   }
-  return(N)
-}
 
 
+  
 ##---
 ## short distance dispersal probabilities
 ##---
@@ -91,53 +95,73 @@ run_sim <- function(
   }
 
 
+  
 ##---
 ## local fruit production
 ##---
-make_fruits <- function(lc.mx, N, N.recruit, fec, K) {
-  # Calculate (N.fruit | N, fec, K) for each cell
-  # Fecundity rates & fruiting probabilities are habitat specific
-  # Assumes no fruit production in first year
-  # N.fruit = (N-N.recruit)*pr(Fruit)*fec
-  # Returns sparse matrix N.f with:
-  #   col(cell.ID, N.fruit)
-  #   nrow = sum(N.fruit != 0)
-  return(N.f)
-}
+  make_fruits <- function(lc.df, N, N.recruit, fec, K) {
+    # Calculate (N.fruit | N, fec, K) for each cell
+    # Fecundity rates & fruiting probabilities are habitat specific
+    # Assumes no fruit production in first year
+    # N.fruit = (N-N.recruit)*pr(Fruit)*fec
+    # Returns sparse matrix N.f with:
+    #   col(cell.ID, N.fruit)
+    #   nrow = sum(N.fruit != 0)
+    
+    
+    return(N.f)
+  }
 
 
+  
 ##---
 ## short distance dispersal
 ##---
-sdd_disperse <- function(lc.mx, N.f, sdd.probs, bird.pref, pr.eat) {
-  # Calculate (N.seeds | N.fruit, sdd.probs, bird.pref, pr.eaten)
-  # Accounts for distance from source cell, bird habitat preference,
-  #   and the proportion of fruits eaten vs dropped
-  # Returns sparse matrix N.sdd with:
-  #   cols(cell.ID, (N.seed = 2*(N.fruit - N.eaten + N.deposited)))
-  #   nrow = sum(N.seed != 0)
-  return(N.seed)
-}
+  sdd_disperse <- function(lc.df, N.f, sdd.probs, bird.pref, pr.eat) {
+    # Calculate (N.seeds | N.fruit, sdd.probs, bird.pref, pr.eaten)
+    # Accounts for distance from source cell, bird habitat preference,
+    #   and the proportion of fruits eaten vs dropped
+    # Returns sparse matrix N.sdd with:
+    #   cols(cell.ID, (N.seed = 2*(N.fruit - N.eaten + N.deposited)))
+    #   nrow = sum(N.seed != 0)
+    return(N.seed)
+  }
 
 
+  
 ##---
 ## long distance dispersal
 ##---
-ldd_disperse <- function(lc.mx, N, N.seed, n.ldd) {
-  # Assign n.ldd random LDD events 
-  # A single seed is added to n.ldd target cells
-  return(N.seed)
-}
+  ldd_disperse <- function(lc.df, N, N.seed, n.ldd) {
+    # Assign n.ldd random LDD events 
+    # A single seed is added to n.ldd target cells
+    return(N.seed)
+  }
 
 
+  
 ##--
 ## seed germination & establishment
 ##--
-new_seedlings <- function(lc.mx, N.seed, pr.est) {
-  # Calculate (N.new | N.seed, pr.est)
-  # Allows for incorporation of management effects
-  return(N.recruit)
-}
+  new_seedlings <- function(lc.df, N.seed, pr.est) {
+    # Calculate (N.new | N.seed, pr.est)
+    # Allows for incorporation of management effects
+    return(N.recruit)
+  }
+
+
+
+
+
+
+
+##########----------------------------------------------------------------------
+##### MUNGING & PLOTTING FUNCTIONS
+##########----------------------------------------------------------------------
+
+
+
+
 
 
 
